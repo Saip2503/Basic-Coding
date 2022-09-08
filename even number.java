e51631742f336c8ed1356ae4b502b22c6c125ea0
Sai809701 <112499446+Sai809701@users.noreply.github.com>
2022-09-09 01:55:17 +0530

    Upload

---
+++ even number.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Scratch {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        if(n%2==0) {
+            System.out.println(n+" is even number");
+        }else {
+            System.out.println(n+" is odd number");
+        }
+    }
+}
